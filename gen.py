079d20847acb08c67e897c422dca6c64abeff0e9
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-11-29 17:25:34 +0100

    add multithreading
    Signed-off-by: cedricfarinazzo <cedric.farinazzo@gmail.com>

--- gen.py
+++ gen.py
@@ -1,12 +1,12 @@
 import os
+import time
+import sys
+import threading
+import multiprocessing
 import PIL
 import PIL.Image as Image
 import PIL.ImageFont as ImageFont
 import PIL.ImageDraw as ImageDraw
-
-#from PIL import Image
-#from PIL import ImageFont
-#from PIL import ImageDraw
 
 
 def seg(img):
@@ -22,10 +22,10 @@ def seg(img):
             x += 1
 
     start[0] = x
-    
+
     x = 0
     y = 0
-    while x != img.size[0]  and y < img.size[1] and \
+    while x != img.size[0] and y < img.size[1] and \
             img.getpixel((x, y)) == (255, 255, 255):
         x += 1
         if img.size[0] == x:
@@ -43,7 +43,7 @@ def seg(img):
             x -= 1
 
     end[0] = x
-    
+
     x = img.size[0] - 1
     y = img.size[1] - 1
     while x != img.size[0] and y > 0 and \
@@ -53,50 +53,94 @@ def seg(img):
             x = img.size[0] - 1
             y -= 1
     end[1] = y
-    
+
     return start, end
 
 
-def gen(letter, fontname, path, fontdir="fonts/", bindir="bin/"):
+def gen(letter, fontname, font, path, fontdir="fonts/", bindir="bin/"):
     if os.path.exists(bindir + path):
         return
-    
+
     img = Image.new('RGB', (512, 512), color=(255, 255, 255))
-    
+
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(fontdir + fontname, 256)
+
     draw.text((0, 0), letter, (0, 0, 0), font=font)
-    
+
     rstart, rend = seg(img)
     cbox = (rstart[0], rstart[1], rend[0], rend[1])
-    
+
     letter = img.crop(box=cbox)
 
     rbox = (0, 0) + letter.size
     mini = letter.resize((28, 28), PIL.Image.ANTIALIAS)
-    
-    #draw.line((rstart[0], rstart[1], rend[0], rstart[1]), fill=(0, 0, 0))
-    #draw.line((rstart[0], rstart[1], rstart[0], rend[1]), fill=(0, 0, 0))
-    #draw.line((rstart[0], rend[1], rend[0], rend[1]), fill=(0, 0, 0))
-    #draw.line((rend[0], rstart[1], rend[0], rend[1]), fill=(0, 0, 0))
-    
+
+    # draw.line((rstart[0], rstart[1], rend[0], rstart[1]), fill=(0, 0, 0))
+    # draw.line((rstart[0], rstart[1], rstart[0], rend[1]), fill=(0, 0, 0))
+    # draw.line((rstart[0], rend[1], rend[0], rend[1]), fill=(0, 0, 0))
+    # draw.line((rend[0], rstart[1], rend[0], rend[1]), fill=(0, 0, 0))
+
     mini.save(bindir + path, quality=100)
+
+
+class GenDataset(threading.Thread):
+    fontlist = []
+    rstart = 0
+    end = 0
+    ascii_table = []
+    fontdir = ""
+    bindir = ""
+
+    def __init__(self, fontlist, rstart, end, ascii_table, fontdir, bindir):
+        threading.Thread.__init__(self)
+        self.fontlist = fontlist
+        self.rstart = rstart
+        self.end = end
+        self.ascii_table = ascii_table
+        self.bindir = bindir
+        self.fontdir = fontdir
+
+    def run(self):
+        font_len = len(self.fontlist)
+        self.end = self.end if self.end <= font_len else font_len
+        for i in range(self.rstart, self.end):
+            fontname = self.fontlist[i]
+            font = ImageFont.truetype(self.fontdir + fontname, 256)
+            for c in self.ascii_table:
+                letter = chr(c)
+                print("ascii: " + str(c) + "  " + letter + " with font " + fontname)
+                gen(letter, fontname, font, str(c) + "_" + fontname + ".png", \
+                    fontdir=self.fontdir, bindir=self.bindir)
 
 
 def main():
     fontdir = "fonts/"
     bindir = "bin/"
     fonts = os.listdir(fontdir)
+    len_fonts = len(fonts)
     ascii_table = [i for i in range(33, 128)]
 
-    for font in fonts:
-        print("  FONT: " + font)
-        for c in ascii_table:
-            letter = chr(c)
-            print("ascii: " + str(c) + "  " + letter)
-            gen(letter, font, str(c) + "_" + font  + ".png", \
-                    fontdir=fontdir, bindir=bindir)
-        print("\n\n")
+    nbcore = multiprocessing.cpu_count()
+    task = len_fonts // nbcore;
+
+    threads = []
+    start = 0
+    end = task
+    while end <= len_fonts:
+        th = GenDataset(fonts, start, end, ascii_table, fontdir, bindir)
+        print(th)
+        threads.append(th)
+        start = end
+        end += task
+
+    for th in threads:
+        th.start()
+
+    for th in threads:
+        th.join()
+
+    print("Done\n")
+
 
 if __name__ == "__main__":
     main()
